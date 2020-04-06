2d7cfffeb48aa64032c7bb14aa894b80617507e6
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-06 18:22:12 -0300

    sts2

--- data store.R
+++ data store.R
@@ -1,2 +1,16 @@
+##Libraries that will be used
 library(ggplot2)
 library(RColorBrewer)
+
+## Creating and extracting the files
+if(!(file.exists("summarySCC_PM25.rds") && 
+     file.exists("Source_Classification_Code.rds"))) { 
+  archiveFile <- "NEI_data.zip"
+  if(!file.exists(archiveFile)) {
+    archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    download.file(url=archiveURL,destfile=archiveFile,method="curl")
+  }  
+  unzip(archiveFile) 
+}
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
