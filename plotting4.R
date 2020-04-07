bed86de224e69d374f4edc420eab4c048f7cdc95
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-06 21:28:48 -0300

    sts14

--- plotting4.R
+++ plotting4.R
@@ -1,9 +1,16 @@
 source("data store.R")
 ##4. Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
 
-SCCcoal <- SCC[grepl("coal", SCC$Short.Name, ignore.case = T),]
-NEIcoal <- NEI[NEI$SCC %in% SCCcoal$SCC,]
-carvao <- aggregate(Emissions ~ year + type, NEIcoal, sum)
+names(SCC)<-gsub("\\.","", names(SCC))
+
+combustao<-grepl(pattern = "comb", SCC$SCCLevelOne, ignore.case = TRUE)
+carv<-grepl(pattern = "coal", SCC$SCCLevelFour, ignore.case = TRUE)
+
+## extracting the SCC in 
+combustaoadd<-SCC[combustao & carv,]$SCC
+valor<-NEIdata[NEIdata$SCC %in% combustaoadd,]
+NIECoalCombustionTotalEm <- aggregate(Emissions~year, valor, sum)
+
 
 ##plotting
 png("plot4.png",width=480,height=480,units="px",bg="transparent")
