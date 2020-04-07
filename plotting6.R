06f8518f940054857f8aeff23ae7f0c0ca68cead
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-06 22:48:54 -0300

    sts17

--- plotting6.R
+++ plotting6.R
@@ -1,15 +1,24 @@
 source("data store.R")
+
 ##6. Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (\color{red}{\verb|fips == "06037"|}fips=="06037"). Which city has seen greater changes over time in motor vehicle emissions?
-baltLAsMotors <- subset(NEI, NEI$fips %in% c("24510","06037") & NEI$type == "ON-ROAD")
-baltLAsMotorsadd <- aggregate(Emissions ~ year + fips, baltLAsMotors, sum)
+dodgen <- SCC[grepl("mobile",SCC$EI.Sector, ignore.case=TRUE),]
+scc_list <- dodgen[grep("vehicle",dodgen$SCC.Level.Two, ignore.case=TRUE),]
+
+gen1 <- SCC[scc_list$SCC,]$SCC
+gen2 <- NEI[NEI$SCC %in% gen1,]
+gen2 <- NEI[NEI$SCC %in% gen1 & NEI$fips %in% c("06037", "24510"),]
+gen2$year <- as.factor(gen2$year)
+dual <- mutate(gen2, City = ifelse(fips == "06037", "Los Angeles, CA", ifelse(fips == "24510", "Baltimore City, MD", 0)))
+info <- aggregate(Emissions~(City+year), dual, sum)
 
 ##plotting
 png("plot6.png",width=480,height=480,units="px",bg="transparent")
-ggplot(baltLAsMotorsadd, aes(year, Emissions, col = fips)) +
-  geom_line() +
-  geom_point() +
-  ggtitle(expression("Baltimore and Los Angeles" ~ PM[2.5] ~ "Motor Vehicle Emissions by Year")) +
-  labs(x = "Year", y = expression(~PM[2.5]~ "Motor Vehicle Emissions") ) +
-  scale_colour_discrete(name = "City", labels = c("Los Angeles", "Baltimore")) +
-  theme(legend.title = element_text(face = "bold"))
+
+ggplot(data=info, aes(y=Emissions, x=year, fill = City)) + 
+  geom_bar(stat="identity", position = "dodge") +
+  scale_y_continuous(breaks = round(seq(500, 7500, by = 500),1)) +
+  ggtitle("Total PM2.5 Emissions - Motor Vehicles", subtitle = "Baltimore City vs Los Angeles") +
+  labs(x = "Year", y = "PM2.5 Emissions (Tons)")
+
 dev.off()
+
