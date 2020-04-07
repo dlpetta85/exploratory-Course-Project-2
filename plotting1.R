83e0da78baba6727e588593cccde8dc952b99335
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-06 21:02:09 -0300

    sts12

--- plotting1.R
+++ plotting1.R
@@ -7,5 +7,11 @@ totEmissões
 
 ## Plotting
 png("plot1.png",width=480,height=480,units="px",bg="transparent")
-barplot((totEmissões$Emissions)/10^6, names.arg=totEmissões$year, xlab="Year", ylab="PM2.5 Emissions (10^6 Tons)", main="PM2.5 Emissions in United States Sources")
+barplot(
+  (totEmissões$Emissions)/10^6,
+  names.arg=totEmissões$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions based on US Sources"
+)
 dev.off()
