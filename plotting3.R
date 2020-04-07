e4f0a90e39b082661ca82ac1a0966da79f9efb69
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-06 21:14:07 -0300

    sts14

--- plotting3.R
+++ plotting3.R
@@ -1,21 +1,19 @@
 source("data store.R")
+
 ##3.Of the four types of sources indicated by the \color{red}{\verb|type|}type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.
-
-
-baltimore <- subset(NEI, NEI$fips == "24510")
-baltimoreTipo <- aggregate(Emissions ~ year + type, baltimore, sum)
-
+NEIBal<-subset(NEIdata, fips == "24510")
+totbal <- aggregate(Emissions ~ year, NEIBal, sum)
+totbal
 
 ##plotting
 png("plot3.png",width=480,height=480,units="px",bg="transparent")
 
-ggplot(baltimoreTipo, aes(year, Emissions, col = type)) +
-  geom_line() +
-  geom_point() +
-  ggtitle(expression("Total in Baltimore " ~ PM[2.5] ~ "Emissions by Type and Year")) +
-  ylab(expression("Total in Baltimore " ~ PM[2.5] ~ "Emissions")) +
-  xlab("Year") +
-  scale_colour_discrete(name = "Type of sources") +
-  theme(legend.title = element_text(face = "bold"))
+A <- ggplot(aes(x = year, y = Emissions, fill=type), data=NEIdataBaltimore)
+A + geom_bar(stat="identity")+
+  facet_grid(.~type)+
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City from 1999 to 2008 based on source Types"))+
+  guides(fill=FALSE)
 
 dev.off()
+
